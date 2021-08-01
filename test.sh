03ebbb45570a38140b011370f8a73c2acfbce774
Sabid <54b1d@users.noreply.github.com>
2021-08-01 12:09:26 +0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,10 @@ pwd
 cd ~/work/
 pwd
 ls
-#wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
-#tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz
+tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz dtmate
+ls
+ls dtmate
 #rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
 #./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
 #./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
